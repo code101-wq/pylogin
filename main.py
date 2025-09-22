bafc3025e38d401e2f1b43ba1e33ec7754c80872
Lobga Julius <85566831+code101-wq@users.noreply.github.com>
2025-09-22 23:39:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from bson.objectid import ObjectId
 from werkzeug.utils import secure_filename
 
 # Set up connection
-MONGO_URI = os.environ.get('MONGO_URI')
-client = MongoClient(MONGO_URI)
+# MONGO_URI = os.environ.get('MONGO_URI')
+# client = MongoClient(MONGO_URI)
+client = MongoCLient('mongodb+srv://lobgafomboh_db_user:Trend123@intro.vd3ychr.mongodb.net/?retryWrites=true&w=majority&appName=Intro')
 db = client["pythonlogin"]
 accounts = db['accounts']
 
