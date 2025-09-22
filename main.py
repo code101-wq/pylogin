647c25823f0fbb59a2de6587c4d069efff0164e7
Lobga Julius <85566831+code101-wq@users.noreply.github.com>
2025-09-22 12:41:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_sqlalchemy import SQLAlchemy
-import re, hashlib
-import mysql.connector as mysql
+import re, hashlib, os
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from werkzeug.utils import secure_filename
 
+# Set up connection
+client = MongoClient("mongodb://localhost:27017")
+db = client["pythonlogin"]
+accounts = db['accounts']
+
+# Define the upload folder and allowed file extensions
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # This is the secret key below, can be anything
 app.secret_key = 'Trendxyz'
 
-# Enter database connection details
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Trend123!@localhost/pythonlogin'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Design sample document to put in the database
+sample_account = {
+    'username' : 'testuser',
+    'email' : 'test@example.com',
+    'password' : 'testuser',
+    'profile_picture': 'placeholder.jpg'  # Added a placeholder
+}
 
-# Initialize SQLAlchemy
-db = SQLAlchemy(app)
+# Check if sample account exists and add it if it's not there
+query = {
+    'username' : 'testuser'
+}
+found_account = accounts.find_one(query)
+if not found_account:
+    # Hash the password for the sample account
+    hash_object = hashlib.sha1(sample_account['password'].encode() + app.secret_key.encode())
+    sample_account['password'] = hash_object.hexdigest()
+    accounts.insert_one(sample_account)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # This route redirects the base URL to the login page
 @app.route('/')
@@ -23,139 +49,128 @@ def root():
 # localhost:5000/pythonlogin/ will be the login page which will use both GET and POST requests
 @app.route('/pythonlogin/', methods=['GET', 'POST'])
 def login():
-    # Output a message if something goes wrong...
     msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
-        # Retrieve the hashed password
-    
-        #Neccessary but not now
-        hash = password + app.secret_key
-        hash = hashlib.sha1(hash.encode())
-        password = hash.hexdigest()
-    
-        # Check if account exists using SQLAlchelmy
-        try:
-            conn = db.engine.connect().connection
-            cursor = conn.cursor(dictionary = True)
-            cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
-            # Fetch one record and return the result
-            account = cursor.fetchone()
-        except mysql.connector.Error as err:
-            msg = f"Database error: {err}"
-            account = None
-        finally:
-            if conn:
-                conn.close()
+        
+        # Hash the password
+        hash_object = hashlib.sha1(password.encode() + app.secret_key.encode())
+        hashed_password = hash_object.hexdigest()
 
-        # If account exists in accounts table in out database
+        query = {
+            'username': username,
+            'password': hashed_password
+            }
+        
+        account = accounts.find_one(query)
+        
         if account:
-            # Create session data, we can access this data in other routes
             session['loggedin'] = True
-            session['id'] = account['id']
             session['username'] = account['username']
-
-            # Redirect to home page
+            session['id'] = str(account['_id'])
             return redirect(url_for('home'))
         else:
-            # Account doesn't exist or username/password incorrect
             msg = 'Incorrect username/password!'
-    # Show the login form with message (if any)
+            
     return render_template('index.html', msg=msg)
 
 @app.route('/pythonlogin/logout')
 def logout():
-    # Remove session data, this will log the user out
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('username', None)
-
-    # Redirect to login page
     return redirect(url_for('login'))
 
 @app.route('/pythonlogin/register', methods=['GET', 'POST'])
 def register():
-    # Output messagew if something goes wrong...
     msg = ''
-    # Check if "username", "password" and "email" POST rquests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
 
-        # Perform validation
-        try:
-            conn = db.engine.connect().connection
-            cursor = conn.cursor(dictionary = True)
-            cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-            # Fetch one record and return the result
-            account = cursor.fetchone()
-            # If account exists show error and validation checks
-            if account:
-                msg = 'Account already exists!'
-            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-                msg = "Invalid email address!"
-            elif not re.match(r'[A-Za-z0-9]+', username):
-                msg = 'Username must contain only characters and numbers!'
-            elif not username or not password or not email:
-                msg = "Please fill out the form!"
-            else:
-                # Hash the password
-                hash = password + app.secret_key
-                hash = hashlib.sha1(hash.encode())
-                password = hash.hexdigest()
-                # Account doesn't exist, and the form data is valid, so insert the new account into the accounts table
-                cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email))
-                conn.commit()
-                msg = 'You have successfully registered!'
-                return redirect(url_for('login'))
-        except mysql.connector.Error as err:
-            msg = f"Database error: {err}"
-        finally:
-            if conn:
-                conn.close()
+        query = {
+            'username' : username
+        }
+        account = accounts.find_one(query)
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = "Invalid email address!"
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = "Username must contain only characters and numbers!"
+        elif not username or not password or not email:
+            msg = "Please fill out the form!"
+        else:
+            # Hash the password
+            hash_object = hashlib.sha1(password.encode() + app.secret_key.encode())
+            hashed_password = hash_object.hexdigest()
 
+            accounts.insert_one(
+                {
+                    "username" : username,
+                    "password" : hashed_password,
+                    "email" : email,
+                    "profile_picture": 'placeholder.jpg'
+                }
+            )
+            msg = 'You have successfully registered!'
+            return redirect(url_for('login'))
     elif request.method == 'POST':
-        # Form is empty... (no POST data)
         msg = 'Please fill out the form!'
-    # Show registration form with message (if any)
+            
     return render_template('register.html', msg=msg)
 
-# localhost:5000/pythonlogin/home - this will be the home page, only accessible for logged in users
 @app.route('/pythonlogin/home')
 def home():
-    # Check if the user is logged in
     if 'loggedin' in session:
-        # User is loggedin show them the home page
         return render_template('home.html', username=session['username'])
-    # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
-# localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for logged in users
 @app.route('/pythonlogin/profile')
 def profile():
-    # Check if the user is logged in
     if 'loggedin' in session:
-        # We need all the account info for the user so we can display it on the profile page
-        try:
-            conn = db.engine.connect().connection
-            cursor = conn.cursor(dictionary = True)
-            cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
-            account = cursor.fetchone()
-        except mysql.connector.Error as err:
-            print(f"Database error: {err}")
-            account = None
-        finally:
-            if conn:
-                conn.close()
-        # Show the profile page with account info
+        account = accounts.find_one({"_id": ObjectId(session['id'])})
         return render_template('profile.html', account=account)
-    # User is not logged in redirect to login page
     return redirect(url_for('login'))
+
+@app.route('/pythonlogin/profile_update', methods=['GET', 'POST'])
+def profile_update():
+    if 'loggedin' in session:
+        msg = ''
+        account = accounts.find_one({"_id": ObjectId(session['id'])})
+
+        if request.method == 'POST':
+            # Check if the post request has the file part
+            if 'profile_picture' not in request.files:
+                msg = 'No file part'
+                return render_template('profile_update.html', msg=msg, account=account)
+            file = request.files['profile_picture']
+            
+            # If the user does not select a file, the browser submits an empty file without a filename
+            if file.filename == '':
+                msg = 'No selected file'
+                return render_template('profile_update.html', msg=msg, account=account)
+
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                # Save the file
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                
+                # Update the profile picture path in the database
+                accounts.update_one(
+                    {"_id": ObjectId(session['id'])},
+                    {"$set": {"profile_picture": filename}}
+                )
+                
+                return redirect(url_for('profile'))
+            else:
+                msg = 'Invalid file type'
+
+        return render_template('profile_update.html', msg=msg, account=account)
+    return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
